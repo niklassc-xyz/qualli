5bdcbd9d0c206afe32390ee0c28a8760a6da4dce
niklassc <niklassc@student.ethz.ch>
2025-04-10 20:53:03 +0200

    switch UnlockBase to ResourceManager

--- datafiles/javascript/globals.js
+++ datafiles/javascript/globals.js
@@ -1,7 +1,6 @@
 // TODO remove all globals
 
 // Load Images from HTML-Ressources
-export var spr_Erde = document.getElementById("img_Erde");
 export var sprMedalGold = document.getElementById("medalGold");
 export var sprMedalSilver = document.getElementById("medalSilver");
 export var sprMedalBronze = document.getElementById("medalBronze");
