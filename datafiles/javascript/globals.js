ceb3027ea78a1ba69cede3895b97fbcbe20282f0
niklassc <niklassc@student.ethz.ch>
2025-04-10 20:31:17 +0200

    switch bubble to resource manager

--- datafiles/javascript/globals.js
+++ datafiles/javascript/globals.js
@@ -2,7 +2,6 @@
 
 // Load Images from HTML-Ressources
 export var spr_Erde = document.getElementById("img_Erde");
-export var spr_Planet = document.getElementById("bubble"); // TODO rename
 export var sprMedalGold = document.getElementById("medalGold");
 export var sprMedalSilver = document.getElementById("medalSilver");
 export var sprMedalBronze = document.getElementById("medalBronze");
