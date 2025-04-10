ed00b4ffdd944deb7c5d1c2ddfabcab034eed0dd
niklassc <niklassc@student.ethz.ch>
2025-04-10 22:23:34 +0200

    switch LevelButton to ResourceManager

--- datafiles/javascript/globals.js
+++ datafiles/javascript/globals.js
@@ -1,7 +1,4 @@
 // TODO remove all globals
 
 // Load Images from HTML-Ressources
-export var sprMedalGold = document.getElementById("medalGold");
-export var sprMedalSilver = document.getElementById("medalSilver");
-export var sprMedalBronze = document.getElementById("medalBronze");
 export var sprLock = document.getElementById("lock");
