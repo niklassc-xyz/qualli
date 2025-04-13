27b2067e93ce5254f105ee867ff6ba6e90299540
niklassc <niklassc@student.ethz.ch>
2025-04-13 19:36:01 +0200

    move chooseRandom to engine

--- datafiles/javascript/functions.js
+++ datafiles/javascript/functions.js
@@ -1,15 +1,3 @@
-/**
- * Returns a random element
- *
- * @param {Array.<T>} arr - Input array to choose element from
- * @returns {T} The random element from `arr`
- */
-export function chooseRandom(arr) {
-	let ri = Math.floor(arr.length * Math.random());
-	return arr[ri];
-
-}
-
 export function radToDeg(rad) {
 	return rad * (180 / Math.PI);
 }
