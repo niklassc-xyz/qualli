5eb40f3c85e9af271d1a6648b0f97a0010c7bdbd
niklassc <niklas@schmatloch.de>
2025-04-13 21:31:57 +0200

    initialization in separate file

---
+++ initGame.js
@@ -0,0 +1,21 @@
+import Settings from "./datafiles/javascript/parapluie/Settings.js";
+import ProgressManager from "./datafiles/javascript/appEtc/ProgressManager.js";
+
+import startpage from "./datafiles/javascript/rooms/Startpage.js";
+import Game from "./datafiles/javascript/parapluie/Game.js";
+
+class Qualli extends Game {
+	constructor(initalRoom, fps=60) {
+		super(initalRoom, fps);
+	}
+}
+
+
+// TODO remove → move overlays out of index.html
+window.Settings = Settings;
+window.ProgressManager = ProgressManager;
+
+window.onload = () => {
+	var game = new Qualli(startpage);
+	window.game = game;
+}
