bcaf4ab24faaecdde3a7a4ab8198a482e6daa5bb
niklassc <niklas@schmatloch.de>
2025-04-18 00:35:23 +0200

    set checkboxes and game variables after change and initialization

--- datafiles/javascript/Settings.js
+++ datafiles/javascript/Settings.js
@@ -2,11 +2,25 @@ export default class Settings {
 	constructor(g) {
 		this.g = g;
 		this.storage = g.storage;
+
+		// Initialize settings if not in storage yet
 		this.g.storage.setIfNotSet("settingsDebug", false);
 		this.g.storage.setIfNotSet("settingsExperimental", true);
 		this.g.storage.setIfNotSet("settingsScaling", true);
 		this.g.storage.setIfNotSet("settingsMute", true);
 		this.g.storage.setIfNotSet("settingsJellyGlow", true);
+
+		document.getElementById("sDebug").checked = this.getDebug();
+		document.getElementById("sExperimental").checked = this.getExperimental();
+		document.getElementById("sScaling").checked = this.getScaling();
+		document.getElementById("sMute").checked = this.getMute();
+		document.getElementById("sJellyGlow").checked = this.getJellyGlow();
+
+		// Values that are stored in the Game need to be set after each
+		// initialization of the Game
+		this.g.setDebug(this.getDebug());
+		this.g.setScaling(this.getDebug());
+
 	}
 
 	// Show overlay
@@ -20,10 +34,11 @@ export default class Settings {
 	}
 
 	getDebug() {
-		return this.storage.get("settingsDebug");
+		return this.storage.get("settingsDebug") == "true";
 	}
 
 	setDebug(val) {
+		this.g.setDebug(val);
 		this.storage.set("settingsDebug", val);
 	}
 
@@ -35,7 +50,7 @@ export default class Settings {
 	}
 
 	getExperimental() {
-		return this.storage.get("settingsExperimental");
+		return this.storage.get("settingsExperimental") == "true";
 	}
 
 	setExperimental(val) {
@@ -50,10 +65,11 @@ export default class Settings {
 	}
 
 	getScaling() {
-		return this.storage.get("settingsScaling");
+		return this.storage.get("settingsScaling") == "true";
 	}
 
 	setScaling(val) {
+		this.g.setScaling(val);
 		this.storage.set("settingsScaling", val);
 	}
 
@@ -66,7 +82,7 @@ export default class Settings {
 	}
 
 	getJellyGlow() {
-		return this.storage.get("settingsJellyGlow");
+		return this.storage.get("settingsJellyGlow") == "true";
 	}
 
 	setJellyGlow(val) {
@@ -81,17 +97,19 @@ export default class Settings {
 	}
 
 	getMute() {
-		return this.storage.get("settingsMute");
+		return this.storage.get("settingsMute") == "true";
 	}
 
 	setMute(val) {
 		this.storage.set("settingsMute", val);
+		document.getElementById("sMute").checked = val;
+		this.g.resourceManager.setMute(val);
+		console.log(val);
 	}
 
 	toggleMute() {
 		const val = !this.getMute();
 		this.setMute(val);
-		document.getElementById("sMute").checked = val;
 		return val;
 	}
 
