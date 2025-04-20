def49d30742f11914d12c7d25d398b47fd31ebab
niklassc <niklas@schmatloch.de>
2025-04-20 16:51:27 +0200

    implement locking

--- datafiles/javascript/objects/LevelButton.js
+++ datafiles/javascript/objects/LevelButton.js
@@ -1,13 +1,22 @@
 import Button from "../parapluie/objects/util/Button.js";
 
 export default class LevelButton extends Button {
-	constructor(g, text, x, y, width, height, level) {
-		let onClick = () => g.gotoRoom(level, g.room.constructor);
+	constructor(g, text, x, y, width, height, level, locked=false) {
+		let onClick = () => {
+			if (this.locked) {
+				alert("Unlock this level first");
+				return false;
+			}
+			g.gotoRoom(level, g.room.constructor)
+		};
 		super(g, text, x, y, width, height, onClick, false);
 
 		this.level = level;
+		this.locked = locked;
+		this.spriteLock = this.g.resourceManager.getSpriteByPath("datafiles/sprites/lock.png");
 
 		this.won = g.progressManager.getLevelStats(level.name).won;
+
 
 		if (this.won > 3) {
 			this.medal = this.g.resourceManager.getSpriteByPath("datafiles/sprites/medals/gold.png");
@@ -19,22 +28,33 @@ export default class LevelButton extends Button {
 			this.medal = undefined;
 		}
 
-		// TODO lock
-		// TODO calculate font size
+		// TODO calculate font size → in base class
 	}
 
 	draw() {
 		super.draw();
 
-		let mWidth = 21; // Medal width → keep aspect ratio of sprite
-		let mHeight = 32; // Medal height → keep aspect ratio of sprite
-		let mMargin = 6;
+		const mWidth = 21; // Medal width → keep aspect ratio of sprite
+		const mHeight = 32; // Medal height → keep aspect ratio of sprite
+		const mMargin = 6;
 		if (this.medal != undefined) {
+			// TODO move drawImage to painter
 			this.g.painter.ctx.drawImage(this.medal,
 				this.x + this.width - mWidth - mMargin,
 				this.y + mMargin,
 				mWidth,
 				mHeight);
+		}
+
+		if (this.locked) {
+			const lMargin = mMargin;
+			const lHeight = mHeight;
+			const lWidth = lHeight;
+			this.g.painter.ctx.drawImage(this.spriteLock,
+				this.x + this.width - lWidth - lMargin,
+				this.y + lMargin,
+				lWidth,
+				lHeight);
 		}
 	}
 }
