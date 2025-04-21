f89810dd71a8424719faa8f677b2a8ac968efda5
niklassc <niklas@schmatloch.de>
2025-04-21 04:18:26 +0200

    add AI and bubble

--- datafiles/javascript/rooms/levels/LevelTest.js
+++ datafiles/javascript/rooms/levels/LevelTest.js
@@ -1,5 +1,6 @@
 import Level from "./Level.js";
 import KI0 from "../../appEtc/KI0.js";
+import Bubble from "../../objects/Bubble.js";
 import BubbleTemp from "../../objects/BubbleTemp.js";
 
 
@@ -7,8 +8,10 @@ export default class LevelTest extends Level {
 	constructor(g, returnRoom = undefined) {
 		super(g, returnRoom);
 
-		// this.addObject(new KI0(this.g, 2));
+		this.addObject(new KI0(this.g, 1));
 
 		this.addBase(new BubbleTemp(this.g, 640, 360, 1, 3, 100));
+
+		this.addBase(new Bubble(this.g, 1200, 600, 2, 1, 100));
 	}
 }
