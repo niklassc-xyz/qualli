2f72842aa16b93bc7df2ff0f799c3e022d73badb
niklassc <niklas@schmatloch.de>
2025-04-22 23:40:01 +0200

    also show when level was lost

--- datafiles/javascript/rooms/levels/Level.js
+++ datafiles/javascript/rooms/levels/Level.js
@@ -71,6 +71,18 @@ export default class Level extends Room {
 			console.log("Test");
 			this.g.painter.fillText("Won 👑", this.g.roomWidth/2, this.g.roomHeight - textHeight)
 			this.g.painter.strokeText("Won 👑", this.g.roomWidth/2, this.g.roomHeight - textHeight)
+		} else if (this.status === "lost") {
+			this.g.painter.setFillStyle("#f4aaff");
+			this.g.painter.setStrokeStyle("black");
+			this.g.painter.setLineWidth(1);
+			this.g.painter.setFont("30px fnt_Comforta_Bold");
+
+			const text = "Won";
+			const textHeight = 20;
+			console.log("Test");
+			this.g.painter.fillText("Lost 🏳", this.g.roomWidth/2, this.g.roomHeight - textHeight)
+			this.g.painter.strokeText("Lost 🏳", this.g.roomWidth/2, this.g.roomHeight - textHeight)
+
 		}
 	}
 
