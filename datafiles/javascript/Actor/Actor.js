bccf0d0d67472f16ad9777bbe3e00996081aac47
niklassc <niklas@schmatloch.de>
2025-04-24 07:01:48 +0200

    check if lost, show in icon

--- datafiles/javascript/Actor/Actor.js
+++ datafiles/javascript/Actor/Actor.js
@@ -1,15 +1,26 @@
 import GameEntity from "../parapluie/objects/GameEntity.js";
 import Colors from "../appEtc/color/Colors.js";
+import Jelly from "../objects/Jelly.js";
+import Base from "../objects/bases/Base.js";
 
 export default class Actor extends GameEntity {
 	constructor(g, team) {
 		super(g);
 		this.team = team;
 
+		this.lost = false;
 		this._modules = [];
 	}
 
 	step() {
+		if (this.lost)
+			return;
+
+		if (this.checkIfLost()) {
+			this.lost = true;
+			return;
+		}
+
 		for (let i = 0; i < this._modules.length; i++)
 			this._modules[i].stepModule();
 	}
@@ -27,6 +38,14 @@ export default class Actor extends GameEntity {
 		this.g.painter.setFont("16px fnt_Comforta_Light");
 		this.g.painter.fillText(this.team, x, y);
 
+		// Show when Actor lost
+		if (this.lost) {
+			this.g.painter.setLineWidth(3);
+			this.g.painter.setStrokeStyle("#b30");
+			this.g.painter.strokeLine(x-r, y-r, x+r, y+r);
+			this.g.painter.strokeLine(x-r, y+r, x+r, y-r);
+		}
+
 		// Modules
 		const mr = r/2;
 		const mMargin = 2;
@@ -40,5 +59,24 @@ export default class Actor extends GameEntity {
 		module.actor = this;
 		module.team = this.team;
 		this._modules.push(module);
+	}
+
+	// Checks if Actor has lost the game
+	checkIfLost() {
+		// Check if Actor owns Bases
+		if (this.g.room.baseManager.getBasesByTeam(this.team).length !== 0)
+			return false;
+
+		// TODO do this better
+		// Check if Actors has active jellies
+		const entities = this.g.room.entities;
+		for (let i = 0; i < entities; i++) {
+			if (entities[i] instanceof Jelly) {
+				if (entities[i].team === this.team)
+					return false;
+			}
+		}
+
+		return true;
 	}
 }
