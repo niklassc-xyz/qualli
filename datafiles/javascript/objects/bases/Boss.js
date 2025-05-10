25dcef479682be140ed835749f5ddc50a367b691
niklassc <me@niklassc.xyz>
2025-05-11 01:59:16 +0200

    add shark sprites, change boss sprite to shark, flip sprite depending on direction, create sharks instead of jellies

--- datafiles/javascript/objects/bases/Boss.js
+++ datafiles/javascript/objects/bases/Boss.js
@@ -4,31 +4,48 @@ import Jelly from "../Jelly.js";
 
 export default class Boss extends Base {
 	constructor(g, x, y, team=0) {
-		const width = 100;
-		const height = 50;
-		const sprite = "datafiles/sprites/squid.svg";
+		const width = 125;
+		const height = 65;
+
+		const sprite = g.resourceManager.getSpriteByPath("datafiles/sprites/shark.svg");
 		super(g, x, y, width, height, sprite, team);
+
+		this.spriteOriginal = this.sprite;
+		this.spriteFlipped = this.g.resourceManager.getSpriteByPath("datafiles/sprites/sharkFlipped.svg");
+
 
 		this.health = 100;
 		this.chooseRandomTarget();
-		this.setSpeed(3);
+		this.targetSpeed = 7;
+		this.setSpeed(this.targetSpeed);
+		this.acceleration = 0.2;
 	}
 
 	step() {
 		super.step();
 
+		if (this.direction > 90 && this.direction < 270)
+			this.sprite = this.spriteFlipped;
+		else
+			this.sprite = this.spriteOriginal;
+
+		// Accelerate
+		if (this.speed < this.targetSpeed)
+			this.setSpeed(Math.min(this.speed + this.acceleration, this.targetSpeed));
+
 		// Randomly attack
-		if (Math.random() < 0.002)
+		if (Math.random() < 0.004)
 			this.attackRandom();
 
 		// Turn towards target
 		const dirToTarget = math.pointDirection(this.x, this.y, this.targetX, this.targetY);
-		const turnSpeed = 2;
+		const turnSpeed = 7;
 		const positiveTurnDistance = math.mMod(dirToTarget - this.direction, 360); // clockwise
 		const negativeTurnDistance = math.mMod(this.direction - dirToTarget, 360); // anticlockwise
 		if (positiveTurnDistance <= turnSpeed || negativeTurnDistance <= turnSpeed) {
 			this.setDirection(dirToTarget);
 		} else {
+			this.setSpeed(Math.max(0.1, this.speed - 1.6*this.acceleration));
 			if (positiveTurnDistance < negativeTurnDistance) {
 				this.setDirection(this.direction + turnSpeed);
 			} else {
@@ -36,7 +53,7 @@ export default class Boss extends Base {
 			}
 		}
 
-		if (math.distance(this.x, this.y, this.targetX, this.targetY) < 100)
+		if (math.distance(this.x, this.y, this.targetX, this.targetY) < 300)
 			this.chooseRandomTarget();
 	}
 
@@ -65,7 +82,9 @@ export default class Boss extends Base {
 		const ri = Math.floor(bases.length * Math.random());
 		const target = bases[ri];
 
-		for (let i = 0; i < n; i++)
-			this.g.room.addObject(new Jelly(this.g, this.x, this.y, this.team, target, this));
+		for (let i = 0; i < n; i++) {
+			const jelly = this.g.room.addObject(new Jelly(this.g, this.x, this.y, this.team, target, this));
+			jelly.sprite = this.spriteOriginal;
+		}
 	}
 }
