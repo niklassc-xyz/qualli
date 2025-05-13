986c8d69e56ae353a92e0ff1e304b932380bdc15
niklassc <me@niklassc.xyz>
2025-05-13 19:38:22 +0200

    Create DiffuseMissiles instead of Jelly

--- datafiles/javascript/objects/bases/Boss.js
+++ datafiles/javascript/objects/bases/Boss.js
@@ -1,6 +1,6 @@
 import Base from "./Base.js";
 import * as math from "../../parapluie/functions/math.js";
-import Jelly from "../units/Jelly.js";
+import DiffuseMissile from "../units/DiffuseMissile.js";
 
 export default class Boss extends Base {
 	constructor(g, x, y, team=0) {
@@ -45,13 +45,14 @@ export default class Boss extends Base {
 
 		// Turn towards target
 		const dirToTarget = math.pointDirection(this.x, this.y, this.targetX, this.targetY);
-		const turnSpeed = 7;
+		// const turnSpeed = 7;
+		const turnSpeed = 3;
 		const positiveTurnDistance = math.mMod(dirToTarget - this.direction, 360); // clockwise
 		const negativeTurnDistance = math.mMod(this.direction - dirToTarget, 360); // anticlockwise
 		if (positiveTurnDistance <= turnSpeed || negativeTurnDistance <= turnSpeed) {
 			this.setDirection(dirToTarget);
 		} else {
-			this.setSpeed(Math.max(0.1, this.speed - 1.6*this.acceleration));
+			this.setSpeed(Math.max(5.5, this.speed - 1.3*this.acceleration));
 			if (positiveTurnDistance < negativeTurnDistance) {
 				this.setDirection(this.direction + turnSpeed);
 			} else {
@@ -94,17 +95,35 @@ export default class Boss extends Base {
 
 		
 		// TODO add getter/randomGetter to Level.js
+
+		for (let i = 0; i < n; i++) {
 		const bases = this.g.room.baseManager.getBases();
 		const ri = Math.floor(bases.length * Math.random());
 		const target = bases[ri];
 
-		for (let i = 0; i < n; i++) {
-			const jelly = this.g.room.addObject(new Jelly(this.g, this.x, this.y, this.team, target, this));
-			const jellyTargetDir = math.pointDirection(jelly.x, jelly.y, jelly.targetX, jelly.targetY)
+			const missileX = this.x;
+			const missileY = this.y;
+			const jellyTargetDir = math.pointDirection(missileX, missileY, target.x, target.y)
+			let missileSprite;
 			if (jellyTargetDir > 90 && jellyTargetDir < 270)
-				jelly.sprite = this.spriteFlipped;
+				missileSprite = this.spriteFlipped;
 			else
-				jelly.sprite = this.spriteOriginal;
+				missileSprite = this.spriteOriginal;
+
+			const missileWidth = 32;
+			const missileHeight = 21;
+
+			const missile = this.g.room.addObject(new DiffuseMissile(this.g,
+			                                                         missileX,
+			                                                         missileY,
+			                                                         missileSprite,
+			                                                         missileWidth,
+			                                                         missileHeight,
+			                                                         this.team,
+			                                                         target,
+			                                                         this));
+			missile.targetSpeed = this.speed * 1.1;
+			missile.setDirectionSpeed(this.direction, this.speed * 1.1);
 		}
 	}
 
