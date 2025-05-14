5583c89c7df5f2d3e932d385c76871f020f3e8c0
niklassc <me@niklassc.xyz>
2025-05-14 11:27:38 +0200

    add healthbar colors

--- datafiles/javascript/appEtc/color/Colors.js
+++ datafiles/javascript/appEtc/color/Colors.js
@@ -10,4 +10,7 @@ export default class Colors {
 		new Color(56, 142, 60, 0.6),
 		new Color(251, 192, 45, 0.6),
 	];
+
+	static healthbarGreen = new Color(68, 255, 119, 1.0);
+	static healthbarRed = new Color(255, 68, 119, 1.0);
 }
